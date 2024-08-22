d8601157a3be65fb7dab94f5614e62c84ad3780a
Rosset13 <makhadoterrence@gmail.com>
2024-08-22 22:31:01 +0000

    bugg

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
-# Change the OS configuration so that it is possible to login with the
-# holberton user and open a file without any error message.
-
-exec {'OS security config':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+# 1-user_limit.pp
+exec { 'change-os-configuration-for-holberton-user':
+  command => 'echo "holberton soft nofile 65535" >> /etc/security/limits.conf && echo "holberton hard nofile 65535" >> /etc/security/limits.conf',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'grep -qv "holberton" /etc/security/limits.conf',
 }
+
